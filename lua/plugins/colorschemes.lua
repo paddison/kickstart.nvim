df910fa3210a9e140754c4573a23b616f178a713
paddison <patrickbaumann579@gmail.com>
2025-12-08 20:07:23 +0100

    update colortheme

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -5,12 +5,18 @@ return {
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      transparent = true,
+      styles = {
+        sidebars = 'transparent',
+        floats = 'transparent',
+      },
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.cmd.colorscheme 'tokyonight-night'
+      --vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
@@ -44,6 +50,16 @@ return {
     'nordtheme/vim',
     priority = 1000,
     config = function()
+      --vim.cmd.colorscheme 'nord'
+    end,
+  },
+  {
+    'shaunsingh/nord.nvim',
+    priority = 1000,
+    config = function()
+      vim.g.nord_disable_background = true
+      vim.g.nord_italic = false
+      vim.g.nord_bold = false
       vim.cmd.colorscheme 'nord'
     end,
   },
